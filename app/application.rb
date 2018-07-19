68976f04e3338acc51361f2f2c36c643eae127db
Monica Seaman <monica.l.seaman@gmail.com>
2018-07-19 13:13:42 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,15 +3,22 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
-    current_time = Time.now
+    #current_time = Time.now
     #resp.write "#{current_time}\n"
     #resp.write "#{current_time.hour}\n"
 
-    if current_time.hour >= 12
-      resp.write "Good Afternoon!"
-    else
-      resp.write "Good Morning!"
+    #if current_time.hour >= 12
+    #  resp.write "Good Afternoon!"
+    #else
+    #  resp.write "Good Morning!"
+    #end
+
+    req = Rack::Request.new(env)
+
+    @@items.each do |item|
+      resp.write "#{item}\n"
     end
+ 
 
     resp.finish
   end
