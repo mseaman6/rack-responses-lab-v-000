f64f0bf82ec70d9fe00be656d1025c0b1276b45e
Monica Seaman <monica.l.seaman@gmail.com>
2018-07-19 13:20:58 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -21,6 +21,15 @@ class Application
       @@items.each do |item|
         resp.write "#{item}\n"
       end
+    elsif req.path.match(/search/)
+ 
+      search_term = req.params["q"]
+ 
+      if @@items.include?(search_term)
+        resp.write "#{search_term} is one of our items"
+      else
+        resp.write "Couldn't find #{search_term}"
+      end
     else
       resp.write "Path Not Found"
     end
